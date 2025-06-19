ac9f17574b37b3900ea36a005e2da2089ed628f7
RePod <RePod@users.noreply.github.com>
2025-06-19 00:12:05 -0400

    test: victory_song

--- test/test_plando.py
+++ test/test_plando.py
@@ -7,10 +7,10 @@ class TestPlando(MegaMixTestBase):
     song_items = MMC.song_items
 
     def _test_plando(self, exclude: bool = False):
-        # Shuffle song_items, pick out 60, allocate 30 to include/exclude, verify they're not returned.
+        """Shuffle song_items, pick out 60, allocate 30 to include/exclude, verify they're not returned."""
 
         world = self.get_world()
-        self.assertTrue(len(self.song_items) >= 60,f"Minimum 60 MMC song_items expected, got {len(self.song_items)}")
+        self.assertGreaterEqual(len(self.song_items), 60, f"Minimum 60 MMC song_items expected, got {len(self.song_items)}")
 
         items = list(self.song_items)
         world.random.shuffle(items)
@@ -18,8 +18,8 @@ class TestPlando(MegaMixTestBase):
         extras = items[-30:]
         candidates = items[0:30]
 
-        self.assertTrue(len(candidates) == 30,f"30 candidates expected, got {len(candidates)}")
-        self.assertTrue(len(extras) == 30,f"30 extras expected, got {len(extras)}")
+        self.assertEqual(30, len(candidates), f"30 candidates expected, got {len(candidates)}")
+        self.assertEqual(30, len(extras), f"30 extras expected, got {len(extras)}")
 
         if exclude:
             world.options.exclude_songs = ExcludeSongs(candidates)
@@ -29,8 +29,8 @@ class TestPlando(MegaMixTestBase):
         song_pool = world.handle_plando(items)
         overlap = [song for song in candidates if song in song_pool]
 
-        self.assertTrue(len(overlap) == 0, f"0 overlap expected, got {len(overlap)}")
-        self.assertTrue(len(song_pool) == len(extras), f"{len(extras)} remaining in song pool expected, got {len(song_pool)}")
+        self.assertEqual(0, len(overlap), f"0 overlap expected, got {len(overlap)}")
+        self.assertEqual(len(extras), len(song_pool), f"{len(extras)} remaining in song pool expected, got {len(song_pool)}")
 
     def test_plando_include(self):
         self._test_plando()
